9654da55b164cdb92f5ff46a47515921620eb372
Aditya Raj <araj44124@gmail.com>
2025-12-25 18:02:25 +0530

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:5000",
+    baseURL: "https://ai-powered-career-readiness-checker.onrender.com",
 });
 
 export const analyzeResume = (formData) =>
